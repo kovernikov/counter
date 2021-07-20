9ea58303dccca41356cbe6858618fcec4c512111
Alexej <kovernikov@post.cz>
2021-07-21 00:26:45 +0200

    Updates

---
+++ precache-manifest.799a8fb7e87b05ab0d41f575e2f621a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ac509290644c6bf934ccc3aee373ef",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "43099acee986b59accbf",
+    "url": "/counter/static/css/main.95e4b119.chunk.css"
+  },
+  {
+    "revision": "80304f8ac46b530f14a6",
+    "url": "/counter/static/js/2.c573d4fa.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/counter/static/js/2.c573d4fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43099acee986b59accbf",
+    "url": "/counter/static/js/main.17e1c77b.chunk.js"
+  },
+  {
+    "revision": "f935969679ad770c1b38",
+    "url": "/counter/static/js/runtime-main.e74b9d66.js"
+  }
+]);
